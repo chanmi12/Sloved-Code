9b4cacf9950e7723854290ed620804b3892f1f5e
chanmi12 <126431868+chanmi12@users.noreply.github.com>
2024-07-24 13:55:38 +0900

    [level 1] Title: 최소직사각형, Time: 0.75 ms, Memory: 4.96 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+    int width = 0;
+    int height = 0;
+    
+    for(int i = 0; i < sizes.size(); i++){
+        if(sizes[i][0] < sizes[i][1]){
+            int wSize = sizes[i][1];
+            sizes[i][1] = sizes[i][0];
+            sizes[i][0] = wSize;
+        }
+        
+        if(width < sizes[i][0]) width = sizes[i][0];
+        if(height < sizes[i][1]) height = sizes[i][1];
+    }
+    answer = width * height;
+    return answer;
+}
