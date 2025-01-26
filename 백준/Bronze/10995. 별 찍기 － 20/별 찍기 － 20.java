93037782f1cf2008895aef4a27c7bb9d12edc6c6
chanmi12 <126431868+chanmi12@users.noreply.github.com>
2025-01-26 22:53:43 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 368 ms, Memory: 21892 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {	
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		
+		int n=scanner.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<2*n;j++) {
+				if((i+j)%2==0) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+    }
+}
